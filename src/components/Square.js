1ff22c986d8e68f286c4831af4192699c3860340
Ben Mukanirwa <benedictobed20@gmail.com>
2023-10-30 15:08:55 +0000

    Creation of Boxes

---
+++ src/components/Square.js
@@ -0,0 +1,14 @@
+import React from 'react'
+//import { useState } from 'react'
+
+export default function Square({value, onSquareClick}) {
+  //const [value, setValue] = useState(null);
+
+  function handleClick(){
+    //console.log("Clicked")
+    //setValue("X")
+  }
+  return (
+    <button className="square" onClick={onSquareClick}>{value}</button>
+  )
+}
